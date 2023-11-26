bb18cee10a8f0fa5471f9e711b54c4c9ca952f59
Charlisteketee <charlisteketee@gmail.com>
2023-11-26 13:05:23 -0800

    Create a registration handler

--- express_server.js
+++ express_server.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
-const cookieParser = require("cookie-parser");
 const PORT = 8080; // default port 8080
-
+const cookieParser = require("cookie-parser");
 app.set("view engine", "ejs");
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser());
 
 const urlDatabase = {
@@ -14,10 +11,21 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+// Users database
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
 
+// Function to generate random ID's for URLS and users
 function generateRandomString() {
   const length = 6;
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -28,6 +36,7 @@ function generateRandomString() {
   return result;
 };
 
+
 app.get("/urls", (req, res) => {
   const templateVars = { 
     username: req.cookies["username"],
@@ -36,13 +45,44 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = { 
+    username: req.cookies["username"],
+  };
+  res.render("urls_registration", templateVars);
+});
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
+});
+
+
+// Add a POST route to handle registration
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  // generate random user ID
+  const userID = generateRandomString();
+  // Create a new user object
+  const newUser = {
+    id: userID,
+    email: email,
+    password: password
+  };
+ // Store randomly generated userID in users database
+ users[userID] = newUser;
+ // Set a cookie with the userID
+ res.cookie("user_ID", userID);
+ res.redirect("/urls");
 });
 
 // Add a POST route to handle login 
 app.post("/login", (req, res) => {
+  console.log(req.body); // Log the request body to the console
+
   const username = req.body.username;
 
   // Set a cookie with the username
@@ -120,11 +160,9 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
-
- 
 
 // in separate terminal or browser type curl http://localhost:8080/urls.json to see JSON representation of the 'urlDatabase'
 // or /hello to see hello world (world in bold)
